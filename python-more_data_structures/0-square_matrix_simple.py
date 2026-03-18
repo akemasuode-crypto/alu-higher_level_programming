e939f50eb5289700f0597c685a93e3fc54bc14c6
akemasuode-crypto <a.kemasuode@alustudent.com>
2026-03-18 13:37:09 +0200

    correction

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     if not matrix:
-        print()
-    return [[element * element for element in row] for row in matrix]
+        return None
+    return [[num * num for num in row] for row in matrix]]
